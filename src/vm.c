babd271d141a9c913ac37c72e8afb74bc876ed37
Vinz2008 <vincentbidarddelanoe@gmail.com>
2025-01-24 22:47:50 +0100

    start working on VM

--- src/vm.c
+++ src/vm.c
@@ -1,17 +1,21 @@
 #include "vm.h"
 
 void run_vm(Bytecode bytecode){
+    VM vm = (VM){
+        .bytecode = bytecode,
+        .instruction_pointer = bytecode.bytecode_array.bytecode,
+    };
 
-    int instruction_pos = 0;
-    while (instruction_pos < bytecode.bytecode_array.length){
-        switch (bytecode.bytecode_array.bytecode[instruction_pos])
+    vm.stack_top = vm.stack;
+    while (vm.instruction_pointer < vm.bytecode.bytecode_array.bytecode + vm.bytecode.bytecode_array.length){
+        switch (*vm.instruction_pointer)
         {
             // TODO
         
         default:
             break;
         }
-        instruction_pos++;
+        vm.instruction_pointer += 1; // TODO : increase of the size of an instruction (or do it in the switch if the instructions are of variable length)
     }
     // TODO
 }
--- src/vm.h
+++ src/vm.h
@@ -6,6 +6,7 @@ typedef struct {
     Bytecode bytecode;
     uint8_t* instruction_pointer;
     Value stack[STACK_MAX]; // TODO : make it dynamic
+    Value* stack_top;
 } VM;
 
 void run_vm(Bytecode bytecode);
