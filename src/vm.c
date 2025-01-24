03ef9a866730ee28d2d2c4a27fcc7403a62d00f5
Vinz2008 <vincentbidarddelanoe@gmail.com>
2025-01-24 22:13:53 +0100

    start working on VM

--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,17 @@
 #include "vm.h"
 
 void run_vm(Bytecode bytecode){
+
+    int instruction_pos = 0;
+    while (instruction_pos < bytecode.bytecode_array.length){
+        switch (bytecode.bytecode_array.bytecode[instruction_pos])
+        {
+            // TODO
+        
+        default:
+            break;
+        }
+        instruction_pos++;
+    }
     // TODO
 }
